5e0fab7b889c49ee3a6a18aa0b24d0df825d756e
Jeremy <jeremymdunne@gmail.com>
2018-05-15 17:22:29 -0400

    Working on auto launch

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,63 @@
 #include "BMP180.h"
 #include "math.h"
 #include "SD.h"
-double T, P, p0, a;
+
+#define SD_ERROR_LED PC13
+#define IMU_ERROR_LED PC14
+#define BMP_ERROR_LED PC15
+
+//double T, P, p0, a;
 unsigned long timeStamp;
 
+#define SD_BUFFER_SIZE 1024
 
 File flightData;
 MPU9250 imu;
 BMP180 pressure;
+
+String fileName;
+
+String getFileName(){
+  String base = "fdat";
+  int counter = 0;
+  while(counter < 10){
+    if(!SD.exists(base + String(counter) + ".txt")){
+      return base + String(counter) + ".txt";
+    }
+    else counter ++;
+  }
+  return "";
+}
+
+char writeBuff[SD_BUFFER_SIZE] = {};
+int buffIndex = 0;
+
+void writeData(String toWrite, bool force){
+  if(buffIndex + toWrite.length() + 1 >= SD_BUFFER_SIZE || force){
+    flightData.write(writeBuff,buffIndex);
+    flightData.flush();
+    //Serial.println("Buff Size: " + String(buffIndex));
+    for(int i = 0; i < buffIndex; i ++){
+      writeBuff[i] = '\0';
+    }
+    buffIndex = 0;
+    //if(!force) writeData("Buffer wrote!\n", false);
+    //Serial.println("Writing buff!");
+  }
+  for(uint i = 0; i < toWrite.length(); i ++){
+    writeBuff[buffIndex] = toWrite.charAt(i);
+    buffIndex ++;
+  }
+}
+
 bool initIMU(){
   if(imu.begin(MPU9250_GYRO_RANGE_2000_DPS, MPU9250_ACCEL_RANGE_16_GPS) < 0){
-    Serial.println("IMU init Fail!");
+    //Serial.println("IMU init Fail!");
+    //digitalWrite(IMUFail, HIGH);
     return false;
   }
-  Serial.println("IMU init Success!");
-  Serial.println("Calibrating Gyroscope!");
+  //Serial.println("IMU init Success!");
+  //Serial.println("Calibrating Gyroscope!");
   imu.calibrateGyroOffsets();
   imu.zero();
   imu.calibrateAccel();
@@ -30,62 +73,82 @@ bool initIMU(){
 bool initBMP(){
   if (pressure.begin())
   {
-    Serial.println("BMP 180 INIT SUCCESS!");
+    return true;
   }
-  else
-  {
-    Serial.println("BMP 180 INIT FAILED!");
-    while(true);
-  }
-
+  return false;
 }
 
 void setup() {
     // put your setup code here, to run once:
     delay(5000);
     Serial.begin(115200);
+    pinMode(IMU_ERROR_LED, OUTPUT);
+    pinMode(BMP_ERROR_LED, OUTPUT);
+    pinMode(SD_ERROR_LED, OUTPUT);
+    digitalWrite(IMU_ERROR_LED, LOW);
+    digitalWrite(BMP_ERROR_LED,LOW);
+    digitalWrite(SD_ERROR_LED, LOW);
     if(!initIMU()){
-      Serial.println("IMU INIT FAILED! STOPPING!");
-      while(true);
+      digitalWrite(IMU_ERROR_LED, HIGH);
+      while(true){
+          Serial.println("IMU INIT FAILED! STOPPING!");
+          delay(20);
+
+      }
     }
     Serial.println("IMU INIT SUCCESS!");
-    initBMP();
-    //while(!Serial.available());
-    if(!SD.begin(PA4)){
-
-      //Serial.println("")
-      while(true)Serial.println("SD init failed!");
+    if(!initBMP()){
+        digitalWrite(BMP_ERROR_LED,HIGH);
+        while(true){
+          Serial.println("BMP FAILURE!");
+          delay(20);
+        }
     }
-
-    flightData = SD.open("DATA.txt",FILE_WRITE);
-    flightData.println("Hello World!");
-    //flightData.close();
-    //while(true);
+    if(!SD.begin(SPI_FULL_SPEED,PA4)){
+      digitalWrite(SD_ERROR_LED, HIGH);
+      while(true){
+        Serial.println("SD Begin FAILED!");
+        delay(20);
+      }
+    }
+    fileName = getFileName();
+    if(fileName.equals("")){
+      while(true){
+        Serial.println("SD Card FULL!");
+        delay(20);
+      }
+    }
+    flightData = SD.open(fileName,O_CREAT | O_WRITE);
+    delay(10);
+    if(flightData){
+      Serial.println("Opened!");
+    }
+    else{
+      while(true){
+        Serial.println("Failed to open!" + fileName + "n");
+        delay(20);
+      }
+    }
     timeStamp = millis();
-
 }
+
 //MPU9250_Raw_Data imuData;
 MPU9250_Data imuData;
 float currentAlt = 0;
 long timeStart = 0;
 void loop() {
-    if(millis() < 10000){
-      timeStamp = millis();
-      currentAlt = pressure.altitude(pressure.getPressureAsync());
-      imu.getData(&imuData);
-      //flightData.println("High");
-      flightData.println("@{OX:" + String(imuData.orientation.x) + ";OY:" + String(imuData.orientation.y) + ";OZ:" + String(imuData.orientation.z) + ";TS:" + String(timeStamp) + ";}@");
-      flightData.println("@{PA:" + String(currentAlt) + ";TS:" + String(timeStamp) + ";}");
-      //flightData.close();
-      Serial.println();
-      Serial.println("@{AX:" + String(imuData.accel.x) + ";AY:" + String(imuData.accel.y) + ";AZ:" + String(imuData.accel.z) + ";TS:" + String(timeStamp) + ";}@");
-      Serial.println("@{GX:" + String(imuData.gyro.x) + ";GY:" + String(imuData.gyro.y) + ";GZ:" + String(imuData.gyro.z) + ";TS:" + String(timeStamp)+ ";}@");
-      Serial.println("@{PA:" + String(currentAlt) + ";TS:" + String(timeStamp) + ";}");
-    }
-    else{
-      flightData.close();
-      while(true);
-    }
+    timeStamp = millis();
+    currentAlt = pressure.altitude(pressure.getPressureAsync());
+    imu.getData(&imuData);
+    //flightData.println("High");
+    writeData("@{OX:" + String(imuData.orientation.x) + ";OY:" + String(imuData.orientation.y) + ";OZ:" + String(imuData.orientation.z) + ";TS:" + String(timeStamp) + ";}@\n",false);
+    writeData("@{PA:" + String(currentAlt) + ";TS:" + String(timeStamp) + ";}@\n",false);
+    writeData("@{AX:" + String(imuData.accel.x) + ";AY:" + String(imuData.accel.y) + ";AZ:" + String(imuData.accel.z) + ";TS:" + String(timeStamp) + ";}@\n",false);
+    //flightData.close();
+    //Serial.println();
+    //Serial.println("@{AX:" + String(imuData.accel.x) + ";AY:" + String(imuData.accel.y) + ";AZ:" + String(imuData.accel.z) + ";TS:" + String(timeStamp) + ";}@\n");
+    //Serial.println("@{GX:" + String(imuData.gyro.x) + ";GY:" + String(imuData.gyro.y) + ";GZ:" + String(imuData.gyro.z) + ";TS:" + String(timeStamp)+ ";}@\n");
+    //Serial.println("@{PA:" + String(currentAlt) + ";TS:" + String(timeStamp) + ";}")
     //getAltitude();
     //Serial.println("Altitude: " + String(a));
 
@@ -105,5 +168,5 @@ void loop() {
     }
     */
     //Serial.println("Velocity z: " + String(imuData.velocity.z));
-    delay(2);
+    //delay(2);
 }
